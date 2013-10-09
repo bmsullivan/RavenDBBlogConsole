a716a585c94d49daa90f5229b6277a0d895c30d2
Brian Sullivan <bmsullivan@gmail.com>
2013-10-08 19:50:32 -0500

    Saving blog object

--- RavenDBBlogConsole/Program.cs
+++ RavenDBBlogConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Raven.Client.Document;
 
 namespace RavenDBBlogConsole
 {
@@ -10,6 +11,31 @@ namespace RavenDBBlogConsole
     {
         static void Main(string[] args)
         {
+            var docStore = new DocumentStore()
+            {
+                Url = "http://localhost:8080"
+            };
+
+            docStore.Initialize();
+
+            using (var session = docStore.OpenSession())
+            {
+                var blog = new Blog()
+                           {
+                               Name = "My New Blog",
+                               Author = "Brian"
+                           };
+
+                session.Store(blog);
+                session.SaveChanges();
+            }
         }
+    }
+
+    public class Blog
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
     }
 }
